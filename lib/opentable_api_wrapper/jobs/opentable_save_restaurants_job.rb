de3491692c0d637a4c4b711dfa9a29f9c5b9189c
AlejandroFernandesAntunes <aantunes70@hotmail.com>
2022-07-11 15:49:33 -0300

    Added missing classes

--- lib/opentable_api_wrapper/jobs/opentable_save_restaurants_job.rb
+++ lib/opentable_api_wrapper/jobs/opentable_save_restaurants_job.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'activerecord'
 require 'sidekiq'
 require 'activerecord-import'
 
--- lib/opentable_api_wrapper/jobs/opentable_update_dir_job.rb
+++ lib/opentable_api_wrapper/jobs/opentable_update_dir_job.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'activerecord'
 require 'sidekiq'
 
 class OpentableUpdateDirJob < ApplicationJob
